8477161663c00ba45234dae07a3463b1923d549f
Amo1000 <aymoncoccaro@gmail.com>
2025-05-22 20:52:49 +1000

    updated text and added glow effect

--- image.js
+++ image.js
@@ -1,6 +1,9 @@
 const image = document.getElementById("image");
-const imgtitle = document.getElementById("title");
+const image2 = document.getElementById("image2");
+const link = document.getElementById("src");
 let params = new URLSearchParams(document.location.search);
 let src = params.get("img");
 
+link.textContent = src;
 image.src = src;
+image2.src = src;
